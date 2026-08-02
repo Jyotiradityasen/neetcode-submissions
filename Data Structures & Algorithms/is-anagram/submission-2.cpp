2b0c211837ded4bb0d42a437cfc4df31c605582c
Jyotiraiditya Sen <131677795+Jyotiradityasen@users.noreply.github.com>
2026-08-02 14:52:42 -0400

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map <char,int> s1;
+        map <char,int> t1;
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        for(int i=0; i<s.length();i++)
+        {
+            char letter=s[i];
+            if(s1.find(letter)==s1.end())
+            {
+                s1[letter]=0;
+            }
+            s1[letter]++;
+        }
+
+        for(int i=0; i<t.length();i++)
+        {
+            char letter=t[i];
+            if(t1.find(letter)==t1.end())
+            {
+                t1[letter]=0;
+            }
+            t1[letter]++;
+        }
+        return s1==t1;
+    }
+};
