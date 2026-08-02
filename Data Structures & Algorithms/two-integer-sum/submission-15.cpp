bad5ba9b4bd8c1fd56e11c88925dd09a5c94e265
Jyotiraiditya Sen <131677795+Jyotiradityasen@users.noreply.github.com>
2026-08-02 15:00:53 -0400

    Add: two-integer-sum - submission-15

---
+++ Data Structures & Algorithms/two-integer-sum/submission-15.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int reach = target - nums[i];
+            if (mp.find(reach) != mp.end())
+            {
+                return {mp[reach], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
