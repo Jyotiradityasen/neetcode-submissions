2d4bb348cc6402c652a8843fb78f0128ede8ff73
Jyotiraiditya Sen <131677795+Jyotiradityasen@users.noreply.github.com>
2026-08-12 21:50:14 -0400

    Add: number-of-students-unable-to-eat-lunch - submission-6

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-6.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        queue <int> q;
+        int n = students.size();
+        for (int student:students)
+        {
+            q.push(student);
+        }
+        int res = n;
+        for(int sandwich : sandwiches)
+        {int cnt=0;
+            while(q.front()!=sandwich && cnt < n)
+            {
+                q.push(q.front());
+                q.pop();
+                cnt++;
+            }
+            if(q.front()==sandwich)
+            {
+                q.pop();
+                res--;
+            }
+            else
+            {
+                break;
+            }
+            
+        }
+        return res;
+    }
+};
